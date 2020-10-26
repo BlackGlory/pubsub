e6c38bdd115fa231bcdf910eec8f0dd514f5cca7
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2020-10-27 00:10:24 +0800

    chore: update comments

--- migrations/config/001-initial.sql
+++ migrations/config/001-initial.sql
@@ -6,7 +6,7 @@
 PRAGMA journal_mode = WAL;
 
 -- SQLite 会将VARCHAR(255)转换为TEXT, 将BOOLEAN转换为NUMERIC, 使用这些数据类型是出于可读性考虑
--- pubsub资源本身是松散的, 无需专门创建表
+-- pubsub资源本身是松散的, 没有自己的表
 
 CREATE TABLE pubsub_json_schema (
   pubsub_id   VARCHAR(255) NOT NULL UNIQUE
