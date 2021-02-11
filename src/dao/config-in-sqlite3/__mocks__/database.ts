d6fc7ed99311c74d19a8c4ebd7bf97c908f766fa
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2021-02-11 11:55:08 +0000

    feat: disable auto_vacuum

--- src/dao/config-in-sqlite3/__mocks__/database.ts
+++ src/dao/config-in-sqlite3/__mocks__/database.ts
@@ -1,13 +1,12 @@
 import Database = require('better-sqlite3')
 import type { Database as IDatabase } from 'better-sqlite3'
 import { strict as assert } from 'assert'
-import { enableAutoVacuum, migrateDatabase } from '../utils'
+import { migrateDatabase } from '../utils'
 
 let db: IDatabase
 
 export function openDatabase(): void {
   db = new Database(':memory:')
-  enableAutoVacuum(db)
 }
 
 export async function prepareDatabase(): Promise<void> {
