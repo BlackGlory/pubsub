a1f79313571540c741ba148289544a6759a8c80e
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2021-07-12 08:56:44 +0000

    refactor: replace common-tags with extra-tags

--- src/services/health/index.ts
+++ src/services/health/index.ts
@@ -1,5 +1,4 @@
 import { FastifyPluginAsync } from 'fastify'
-import { stripIndent } from 'common-tags'
 
 export const routes: FastifyPluginAsync = async function routes(server) {
   server.get('/health', (req, reply) => {
