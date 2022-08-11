9de8a1b40a19e94ebe73fd1ba4e15f8660884669
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2022-08-11 15:34:14 +0000

    refactor: improve performance

--- src/dao/config-in-sqlite3/access-control/token.ts
+++ src/dao/config-in-sqlite3/access-control/token.ts
@@ -1,54 +1,55 @@
 import { getDatabase } from '../database'
+import { withLazyStatic, lazyStatic } from 'extra-lazy'
 
-export function getAllNamespacesWithTokens(): string[] {
-  const result = getDatabase().prepare(`
+export const getAllNamespacesWithTokens = withLazyStatic(function (): string[] {
+  const result = lazyStatic(() => getDatabase().prepare(`
     SELECT namespace
       FROM pubsub_token;
-  `).all()
+  `), [getDatabase()]).all()
 
   return result.map(x => x['namespace'])
-}
+})
 
-export function getAllTokens(
+export const getAllTokens = withLazyStatic(function (
   namespace: string
 ): Array<{ token: string, write: boolean, read: boolean }> {
   const result: Array<{
     token: string
     'write_permission': number
     'read_permission': number
-  }> = getDatabase().prepare(`
+  }> = lazyStatic(() => getDatabase().prepare(`
     SELECT token
          , write_permission
          , read_permission
       FROM pubsub_token
      WHERE namespace = $namespace;
-  `).all({ namespace })
+  `), [getDatabase()]).all({ namespace })
 
   return result.map(x => ({
     token: x['token']
   , write: x['write_permission'] === 1
   , read: x['read_permission'] === 1
   }))
-}
+})
 
-export function hasWriteTokens(namespace: string): boolean {
-  const result = getDatabase().prepare(`
+export const hasWriteTokens = withLazyStatic(function (namespace: string): boolean {
+  const result = lazyStatic(() => getDatabase().prepare(`
     SELECT EXISTS(
              SELECT 1
                FROM pubsub_token
               WHERE namespace = $namespace
                 AND write_permission = 1
            ) AS write_tokens_exist
-  `).get({ namespace })
+  `), [getDatabase()]).get({ namespace })
 
   return result['write_tokens_exist'] === 1
-}
+})
 
-export function matchWriteToken({ token, namespace }: {
+export const matchWriteToken = withLazyStatic(function ({ token, namespace }: {
   token: string
   namespace: string
 }): boolean {
-  const result = getDatabase().prepare(`
+  const result = lazyStatic(() => getDatabase().prepare(`
     SELECT EXISTS(
              SELECT 1
                FROM pubsub_token
@@ -56,58 +57,60 @@ export function matchWriteToken({ token, namespace }: {
                 AND token = $token
                 AND write_permission = 1
            ) AS matched
-  `).get({ token, namespace })
+  `), [getDatabase()]).get({ token, namespace })
 
   return result['matched'] === 1
-}
+})
 
-export function setWriteToken({ token, namespace }: {
+export const setWriteToken = withLazyStatic(function ({ token, namespace }: {
   token: string
   namespace: string
 }): void {
-  getDatabase().prepare(`
+  lazyStatic(() => getDatabase().prepare(`
     INSERT INTO pubsub_token (token, namespace, write_permission)
     VALUES ($token, $namespace, 1)
         ON CONFLICT (token, namespace)
         DO UPDATE SET write_permission = 1;
-  `).run({ token, namespace })
-}
+  `), [getDatabase()]).run({ token, namespace })
+})
 
-export function unsetWriteToken({ token, namespace }: {
+export const unsetWriteToken = withLazyStatic(function (params: {
   token: string
   namespace: string
 }): void {
-  const db = getDatabase()
-  db.transaction(() => {
-    db.prepare(`
+  lazyStatic(() => getDatabase().transaction(({ token, namespace }: {
+    token: string
+    namespace: string
+  }) => {
+    lazyStatic(() => getDatabase().prepare(`
       UPDATE pubsub_token
          SET write_permission = 0
        WHERE token = $token
          AND namespace = $namespace;
-    `).run({ token, namespace })
+    `), [getDatabase()]).run({ token, namespace })
 
     deleteNoPermissionToken({ token, namespace })
-  })()
-}
+  }), [getDatabase()])(params)
+})
 
-export function hasReadTokens(namespace: string): boolean {
-  const result = getDatabase().prepare(`
+export const hasReadTokens = withLazyStatic(function (namespace: string): boolean {
+  const result = lazyStatic(() => getDatabase().prepare(`
     SELECT EXISTS(
              SELECT 1
                FROM pubsub_token
               WHERE namespace = $namespace
                 AND read_permission = 1
            ) AS read_tokens_exist
-  `).get({ namespace })
+  `), [getDatabase()]).get({ namespace })
 
   return result['read_tokens_exist'] === 1
-}
+})
 
-export function matchReadToken({ token, namespace }: {
+export const matchReadToken = withLazyStatic(function ({ token, namespace }: {
   token: string
   namespace: string
 }): boolean {
-  const result = getDatabase().prepare(`
+  const result = lazyStatic(() => getDatabase().prepare(`
     SELECT EXISTS(
              SELECT 1
                FROM pubsub_token
@@ -115,40 +118,51 @@ export function matchReadToken({ token, namespace }: {
                 AND token = $token
                 AND read_permission = 1
            ) AS matched
-  `).get({ token, namespace })
+  `), [getDatabase()]).get({ token, namespace })
 
   return result['matched'] === 1
-}
+})
 
-export function setReadToken({ token, namespace }: { token: string; namespace: string }) {
-  getDatabase().prepare(`
+export const setReadToken = withLazyStatic(function ({ token, namespace }: {
+  token: string
+  namespace: string
+}): void {
+  lazyStatic(() => getDatabase().prepare(`
     INSERT INTO pubsub_token (token, namespace, read_permission)
     VALUES ($token, $namespace, 1)
         ON CONFLICT (token, namespace)
         DO UPDATE SET read_permission = 1;
-  `).run({ token, namespace })
-}
+  `), [getDatabase()]).run({ token, namespace })
+})
 
-export function unsetReadToken({ token, namespace }: { token: string; namespace: string }) {
-  const db = getDatabase()
-  db.transaction(() => {
-    db.prepare(`
+export const unsetReadToken = withLazyStatic(function (params: {
+  token: string
+  namespace: string
+}): void {
+  lazyStatic(() => getDatabase().transaction(({ token, namespace }: {
+    token: string
+    namespace: string
+  }) => {
+    lazyStatic(() => getDatabase().prepare(`
       UPDATE pubsub_token
          SET read_permission = 0
        WHERE token = $token
          AND namespace = $namespace;
-    `).run({ token, namespace })
+    `), [getDatabase()]).run({ token, namespace })
 
     deleteNoPermissionToken({ token, namespace })
-  })()
-}
+  }), [getDatabase()])(params)
+})
 
-function deleteNoPermissionToken({ token, namespace }: { token: string, namespace: string }) {
-  getDatabase().prepare(`
+const deleteNoPermissionToken = withLazyStatic(function ({ token, namespace }: {
+  token: string
+  namespace: string
+}): void {
+  lazyStatic(() => getDatabase().prepare(`
     DELETE FROM pubsub_token
      WHERE token = $token
        AND namespace = $namespace
        AND read_permission = 0
        AND write_permission = 0;
-  `).run({ token, namespace })
-}
+  `), [getDatabase()]).run({ token, namespace })
+})
--- src/dao/config-in-sqlite3/json-schema/json-schema.ts
+++ src/dao/config-in-sqlite3/json-schema/json-schema.ts
@@ -1,38 +1,39 @@
 import { getDatabase } from '../database'
+import { withLazyStatic, lazyStatic } from 'extra-lazy'
 
-export function getAllNamespacesWithJsonSchema(): string[] {
-  const result = getDatabase().prepare(`
+export const getAllNamespacesWithJsonSchema = withLazyStatic(function (): string[] {
+  const result = lazyStatic(() => getDatabase().prepare(`
     SELECT namespace
       FROM pubsub_json_schema
-  `).all()
+  `), [getDatabase()]).all()
 
   return result.map(x => x['namespace'])
-}
+})
 
-export function getJsonSchema(namespace: string): string | null {
-  const result = getDatabase().prepare(`
+export const getJsonSchema = withLazyStatic(function (namespace: string): string | null {
+  const result = lazyStatic(() => getDatabase().prepare(`
     SELECT json_schema FROM pubsub_json_schema
      WHERE namespace = $namespace;
-  `).get({ namespace })
+  `), [getDatabase()]).get({ namespace })
 
   return result ? result['json_schema'] : null
-}
+})
 
-export function setJsonSchema({
+export const setJsonSchema = withLazyStatic(function ({
   namespace
 , schema
 }: { namespace: string; schema: string }): void {
-  getDatabase().prepare(`
+  lazyStatic(() => getDatabase().prepare(`
     INSERT INTO pubsub_json_schema (namespace, json_schema)
     VALUES ($namespace, $schema)
         ON CONFLICT(namespace)
         DO UPDATE SET json_schema = $schema;
-  `).run({ namespace, schema })
-}
+  `), [getDatabase()]).run({ namespace, schema })
+})
 
-export function removeJsonSchema(namespace: string): void {
-  getDatabase().prepare(`
+export const removeJsonSchema = withLazyStatic(function (namespace: string): void {
+  lazyStatic(() => getDatabase().prepare(`
     DELETE FROM pubsub_json_schema
      WHERE namespace = $namespace;
-  `).run({ namespace })
-}
+  `), [getDatabase()]).run({ namespace })
+})
