f1bd18c0b9eaa0380ef35bd28370da25e7dca294
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2022-09-07 05:34:26 +0000

    refactor: use you-died

--- src/index.ts
+++ src/index.ts
@@ -2,16 +2,11 @@ import { go } from '@blackglory/go'
 import * as ConfigInSqlite3 from '@dao/config-in-sqlite3/database'
 import { buildServer } from './server'
 import { PORT, HOST, NODE_ENV, NodeEnv } from '@env'
-
-process.on('exit', () => {
-  ConfigInSqlite3.closeDatabase()
-})
-process.on('SIGHUP', () => process.exit(128 + 1))
-process.on('SIGINT', () => process.exit(128 + 2))
-process.on('SIGTERM', () => process.exit(128 + 15))
+import { youDied } from 'you-died'
 
 go(async () => {
   ConfigInSqlite3.openDatabase()
+  youDied(() => ConfigInSqlite3.closeDatabase())
   await ConfigInSqlite3.prepareDatabase()
 
   const server = buildServer()
