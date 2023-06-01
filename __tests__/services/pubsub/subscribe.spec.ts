6dcfcbf296ab5aaa8b87b8fa85e102742d2e3df8
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2023-06-01 16:29:28 +0000

    test: refactor

--- __tests__/services/pubsub/subscribe.spec.ts
+++ __tests__/services/pubsub/subscribe.spec.ts
@@ -2,6 +2,7 @@ import { startService, stopService, getAddress } from '@test/utils.js'
 import { fetchEvents } from 'extra-sse'
 import { firstAsync } from 'iterable-operator'
 import { publish } from '@apis/publish.js'
+import { delay } from 'extra-promise'
 
 beforeEach(startService)
 afterEach(stopService)
@@ -11,8 +12,10 @@ test('subscribe', async () => {
   const channel = 'channel'
 
   const iter = fetchEvents(`${getAddress()}/namespaces/${namespace}/channels/${channel}`)
-  setTimeout(() => publish(namespace, channel, 'content'), 500)
-  const result = await firstAsync(iter)
+  const promise = firstAsync(iter)
+  await delay(500)
+  publish(namespace, channel, 'content')
+  const result = await promise
 
   expect(result).toStrictEqual({
     comment: undefined
